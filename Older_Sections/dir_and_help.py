3f64060ec87b7f67732055719ea11fe7baebf7d2
Wagner Faria Barbosa <wagner.barbosa@ufv.br>
2022-12-06 18:29:23 -0300

    First archives of python from Udemy's course.

---
+++ Older_Sections/dir_and_help.py
@@ -0,0 +1,8 @@
+"""
+Utilitários Python para auxiliar na programação.
+
+Dir -> apresenta todos os atributos/propriedades e funções/métodos para determinado tipo de dado ou variável.
+dir(tipo de dado/variável)
+Help -> Apresenta a documentação/como utilizar os atributos/propriendades e funções/métodos disponívels para determinado
+tipo de dado ou variável.
+"""
