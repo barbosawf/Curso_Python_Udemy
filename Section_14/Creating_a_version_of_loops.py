b78a4324cf302b2ec167450ac69a6be048b3e0b1
Wagner Faria Barbosa <wagner.barbosa@ufv.br>
2023-01-30 13:09:40 -0300

    First archives of python from Udemy's course.

--- Section_14/Creating_a_version_of_loops.py
+++ Section_14/Creating_a_version_of_loops.py
@@ -1,3 +1,34 @@
 ﻿"""
 Criando a própria versão de Python
 """
+
+
+def meu_for(iterable):
+    iterable = iter(iterable)
+    while True:
+        try:
+            print(next(iterable))
+        except StopIteration:
+            break
+
+
+meu_for('Wagner\n')
+
+
+def meu_for_2(iterable):
+    l = len(iterable)
+    iterable = iter(iterable)
+    c = 0
+    while c < l:
+        try:
+            print(next(iterable))
+        except StopIteration:
+            break
+        c += 1
+
+
+meu_for_2('Faria')
+print()
+meu_for([1, 3, 4, 5])
+print()
+meu_for_2([9, 5, 4, 3])
