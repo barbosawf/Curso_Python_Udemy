d6863731c20b61a9f7677b9eb90b5ce92be59aef
Wagner Faria Barbosa <wagner.barbosa@ufv.br>
2023-02-02 11:53:38 -0300

    First archives of python from Udemy's course.

--- Section_08/06_args.py
+++ Section_08/06_args.py
@@ -13,24 +13,29 @@ O parâmetro *args utilzado numa função coloca os valores extras informados em
 Lembrando que tuplas são imutáveis.
 """
 
+a = '\033[0;33m'
+b = '\033[0;36m'
+c = '\033[m'
+
 # Exemplo 01
-print("Exemplo 01")
+print(f"{a}Exemplo 01{c}")
 
 
 def soma_todos_numeros(*args):
     return sum(args)
 
 
-print("""
+print(f"""{b}
 def soma_todos_numeros(*args):
-    return sum(args)
+    return sum(args){c}
     """)
+
 print('soma_todos_numeros(): ', soma_todos_numeros())
 print('soma_todos_numeros(1, 2, 3): ', soma_todos_numeros(1, 2, 3))
 print('soma_todos_numeros(1, 2, 3, 4, 5): ', soma_todos_numeros(1, 2, 3, 4, 5))
 
 # Exemplo 02
-print("\nExemplo 02")
+print(f"{a}\nExemplo 02{c}")
 
 
 def soma_todos_numeros(nome, sobrenome, *args):
@@ -38,16 +43,17 @@ def soma_todos_numeros(nome, sobrenome, *args):
     return lista
 
 
-print("""
+print(f"""{b}
 def soma_todos_numeros(nome, email, *args):
-    return list(nome, email, sum(args))
+    return list(nome, email, sum(args)){c}
     """)
-print('soma_todos_numeros(): ', soma_todos_numeros('Agenlina', 'Jolie'))
-print('soma_todos_numeros(1, 2, 3): ', soma_todos_numeros('Agenlina', 'Jolie', 1, 2, 3))
-print('soma_todos_numeros(1, 2, 3, 4, 5): ', soma_todos_numeros('Agenlina', 'Jolie', 1, 2, 3, 4, 5))
+
+print("soma_todos_numeros('Agenlina', 'Jolie'): ", soma_todos_numeros('Agenlina', 'Jolie'))
+print("soma_todos_numeros('Agenlina', 'Jolie', 1, 2, 3): ", soma_todos_numeros('Agenlina', 'Jolie', 1, 2, 3))
+print("soma_todos_numeros('Agenlina', 'Jolie', 1, 2, 3, 4): ", soma_todos_numeros('Agenlina', 'Jolie', 1, 2, 3, 4))
 
 # Exemplo 03
-print("\nExemplo 03")
+print(f"{a}\nExemplo 03{c}")
 
 
 def verifica_info(*args):
@@ -56,52 +62,53 @@ def verifica_info(*args):
     return 'Eu não tenho certeza quem você é...'
 
 
-print("""
+print(f"""{b}
 def verifica_info(*args):
     if 'Geek' in args and 'University' in args:
         return 'Bem-vindo Geek'
-    return 'Eu não tenho certeza quem você é...'
+    return 'Eu não tenho certeza quem você é...'{c}
     """)
+
 print('verifica_info(): ', verifica_info())
 print("verifica_info(1, True, 'University', 'Geek'): ", verifica_info(1, True, 'University', 'Geek'))
 print("verifica_info(1, 'University', 3.453): ", verifica_info(1, 'University', 3.453))
 
 
-# Exemplo 03
-print("\nExemplo 03")
+# Exemplo 04
+print(f"{a}\nExemplo 04{c}")
 
 
 def lista_num(*args):
     return args
 
 
-print("""
+print(f"""{b}
 def lista_num(*args):
-    return args
+    return args{c}
     """)
 
 print('lista_num([1, 2, 3], 4, 5): ', lista_num([1, 2, 3], 4, 5))
 
 
-# Exemplo 04 - Uso de desempacotamento
-print("\nExemplo 04")
+# Exemplo 05 - Uso de desempacotamento
+print(f"{a}\nExemplo 05{c}")
 
 
 def lista_num(*args):
     return sum(args)
 
 
-print("""
+print(f"""{b}
 def lista_num(*args):
-    return args
+    return args{c}
     """)
 
 print('lista_num(*[1, 2, 3], 5, 6): ', lista_num(*[1, 2, 3], 5, 6))
 
 print('O asterisco na frente da lista desempacota a lista. Se não tiver dá erro.')
 
-# Exemplo 05
-print('\nExemplo 05')
+# Exemplo 06
+print(f'{a}\nExemplo 06{c}')
 
 lista = [2, 3, 5]
 print('lista = [2, 3, 5]')
