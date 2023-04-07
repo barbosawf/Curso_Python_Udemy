4507f3be940d26c30868cd9dd4054211f19588db
barbosawf <wagner.barbosa@ufv.br>
2023-04-07 14:00:38 -0300

    First archives of python from Udemy's course.

---
+++ Section_16/A01_object_orientation.py
@@ -0,0 +1,59 @@
+﻿"""
+Programação orientada a objetos - POO
+
+- POO é um paradigma de programação que utiliza mapeamento de objetos do mundo real para modelos computacionais.
+
+Paradigma de programação é a forma/metodologia utilizada para pensar/desenvolver sistemaas.
+Python suporta vários tipos de paradígmas de programação:
+
+Principais elementos da POO
+- Classe: modelo do objeto do mundo real sendo representado computacionamemente.
+- Atributo: característica do objeto.
+- Método: comportamento do objeto (funções)
+- Construtor: método especial utilizado para criar os objetos
+- Objeto: instância da classe.
+
+Se escrevemos as nossas classes, estamos criando o tipo do objeto.
+Exemplos:
+
+numero = 10  # cria um objeto do tipo inteiro
+nome = 'Geek'  # cria um objeto do tipo string.
+
+class Produto:
+    pass
+
+ps4 = Produto()
+
+criou-se um produto chamado ps4 e utilizou-se o construtor da classe Produto para criar ps4.
+
+
+1) Programação orientada a objetos: objetivo é mapear objetos do mundo real para modelos computacionais,
+fazendo com que possamos representar tais objetos com suas características e comportamentos.
+
+Exemplo:
+
+Classe produto pode ter 3 atributos (nome, preço, desconto).
+Métodos são o comportamento do objeto (lembrar de função). O método está dentro de uma classe.
+Uma função dentro da classe é um método.
+
+Construtor também é um método, mas é um método especial. Serve para criar um objeto a partir da classe.
+2) Programação estruturada
+3) Programação funcional
+"""
+
+numero = 10  # cria um objeto do tipo inteiro
+print('numero: ', numero)
+print('type(numero): ', type(numero))
+
+nome = 'Geek'  # cria um objeto do tipo string.
+print('\nnome: ', nome)
+print('type(nome): ', type(nome))
+
+
+class Produto:
+    pass
+
+
+ps4 = Produto()
+print('\nps4: ', ps4)
+print('type(ps4): ', type(ps4))
---
+++ Section_16/A02_classes.py
@@ -0,0 +1,43 @@
+﻿"""
+POO - Classes
+
+Em POO, classes são modelos dos objetos do mundo real sendo representados computacionalmente.
+
+Classes podem conter:
+- Atributos: representam as características do objeto. Pelos atributos conseguimos representar computacionalmente
+os estados de um objeto. No caso da lâmpada, possivelmente, iríamos querer saber a voltagem, a luminosidade,
+se é branca ou amarela, etc.
+
+- Métodos (funções) -> representam os comportamento do objeto, ou seja, as ações que este objeto pode realizar
+no sistema. No caso da lâmpada um comportamento seria ligar e desligar a lâmpada.
+
+
+OBS1: utilizamos a palavra pass em Python quando temos um bloco de código que ainda não está implementado.
+
+OBS2: Quando nomeamos nossas classes em Python utilizamos por convenção o nome com inicial em maiúsculo.
+Se o nome for composto, utiliza-se as iniciais de ambas as palavras em maicúlculo, todas juntas.
+
+OBS3: Classes internas do Python são com letras minúsculas. Ex. int, str, float.
+
+OBS4: Quando estamos planejando um software e definimos quais calasses teremos que ter no sistema,
+chamamos estes objetos que serão mapeados para classes de entidade. Lampada é uma entidade no exemplo abaixo.
+"""
+
+# Imagine que você queira fazer um sistema para automatizar o controle das lâmpadas da sua casa.
+
+
+class Lampada:
+    pass
+
+
+lamp = Lampada()
+
+print('type(lamp): ', type(lamp))
+
+
+class ContaCorrente:
+    pass
+
+
+
+
